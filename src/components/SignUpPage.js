c720357b3f4977c8711c7c3800f39b394d473235
misssteph19 <60268582+misssteph19@users.noreply.github.com>
2022-04-03 16:51:10 -0500

    Added SignUpPage and its link

---
+++ src/components/SignUpPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+/*
+ * TODO: Make the SignUp page and its necessary parts
+ * this is only a placeholder and can be changed/replaced as necessary
+ */
+function SignUpPage() {
+    return (
+        <div>
+            Sign Up Page
+        </div>
+    );
+}
+
+export default SignUpPage;
