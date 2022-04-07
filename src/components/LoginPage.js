43c5e6f67d8c370960b41ce46981798c808f2a3e
Samip <s.poudel@utexas.edu>
2022-04-07 17:21:14 -0500

    frontend commit

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,45 +1,135 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-
-/*
- * TODO: Make the login page and its necessary parts
- * this is only a placeholder and can be changed/replaced as necessary
- */
-function LoginPage() {
-
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [error, setError]= useState("")
-
-
-    return (
-        <div>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                fetch("http://127.0.0.1:5000/check_correct/" + username + "/" + password)
-                        .then(response => 
-                            response.json()
-                        )
-                        .then(data => {
-                            setError(data.error)
-                        })
-                        .catch(error => {
-                            console.log(error)
-                        })
-                }}>
-                <label>Username: 
-                    <input type="text" value ={username} onChange = {(e) => setUsername(e.target.value)}></input>
-                </label>
-                <label>Password: 
-                    <input type="text" value = {password} onChange = {(e) => setPassword(e.target.value)}></input>
-                </label>
-                
-                <input type="submit" value="Login"></input>
-            </form>
-            <label>{error}</label>
-        </div>
-    );
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        EE461L Group 
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default LoginPage; 
+const theme = createTheme();
+
+export default function SignIn() {
+    // const [username, setUsername] = useState("")
+    // const [password, setPassword] = useState("")
+    const [error, setError]= useState("")
+
+    const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get('email'),
+//       password: data.get('password'),
+//     });
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        
+
+        console.log({
+                  username: formData.get('username'),
+                  password: formData.get('password'),
+                });
+        fetch("http://127.0.0.1:5000/check_correct/" + formData.get('username') + "/" + formData.get('password'))
+                .then(response => 
+                    response.json()
+                )
+                .then(data => {
+                    setError(data.error)
+                })
+                .catch(error => {
+                    console.log(error)
+                    
+                })
+    };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            {/* <LockOutlinedIcon /> */}
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            {/* <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> */}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                {/* <Link href="#" variant="body2">
+                  Forgot password?
+                </Link> */}
+              </Grid>
+              <Grid item>
+                <Link href="signup" variant="body2">
+                  {"Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+      </ThemeProvider>
+  );
+}
