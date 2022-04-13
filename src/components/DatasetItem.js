8e32f88676a0b9de787f0e4d132433d72abd7ddd
Casey K <caseykim5515@utexas.edu>
2022-04-13 16:37:01 -0500

    bug fix to render items

--- src/components/DatasetItem.js
+++ src/components/DatasetItem.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
 class DatasetItem extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            recordnum: 0
+        }
+    }
+
+    componentWillMount() {
+        fetch("/dataset/" + this.props.dskey) // use "http://127.0.0.1:5000/dataset/" + inputCode for local'
+        .then(response => 
+          response.json()
+        )
+        .then(data => {
+          console.log(data.recordNum)
+          this.setState({
+            recordnum: data.recordNum
+          })
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
 
     render() {
         return(
             <Stack direction='column' alignItems="flex-start" justifyContent='flex-start' spacing={2}>
                 <Stack direction='row' alignItems="flex-start" justifyContent='flex-start' spacing={3}>
-                    <a target="_blank" href={this.props.dblink}>
-                        {this.props.dbname}
+                    <a target="_blank" href={this.props.dslink}>
+                        {this.props.dsname}
                     </a>
 
                     <a target="_blank" href={this.props.dllink}>
@@ -22,7 +40,7 @@ class DatasetItem extends React.Component {
                 </Stack>
 
                 <div>
-                    {"Number of Records: " + this.props.recordnum}
+                    {"Number of Records: " + this.state.recordnum}
                 </div>
 
             </Stack>
--- src/components/DatasetsPage.js
+++ src/components/DatasetsPage.js
@@ -5,51 +5,38 @@ import { Stack } from '@mui/material';
 /*
  * page for datasets; can view, download, and see how many records there are for each dataset
  */
-const dblink1='https://physionet.org/content/adfecgdb/1.0.0/'
+
+//links for downloading and viewing datasets
+const dslink1='https://physionet.org/content/adfecgdb/1.0.0/'
 const dllink1='https://physionet.org/static/published-projects/adfecgdb/abdominal-and-direct-fetal-ecg-database-1.0.0.zip'
-const dblink2='https://physionet.org/content/aftdb/1.0.0/'
+const dslink2='https://physionet.org/content/aftdb/1.0.0/'
 const dllink2='https://physionet.org/static/published-projects/aftdb/af-termination-challenge-database-1.0.0.zip'
-const dblink3='https://physionet.org/content/aami-ec13/1.0.0/'
+const dslink3='https://physionet.org/content/aami-ec13/1.0.0/'
 const dllink3='https://physionet.org/static/published-projects/aami-ec13/ansiaami-ec13-test-waveforms-1.0.0.zip'
-const dblink4='https://physionet.org/content/apnea-ecg/1.0.0/'
+const dslink4='https://physionet.org/content/apnea-ecg/1.0.0/'
 const dllink4='https://physionet.org/static/published-projects/apnea-ecg/apnea-ecg-database-1.0.0.zip'
-const dblink5='https://physionet.org/content/auditory-eeg/1.0.0/'
+const dslink5='https://physionet.org/content/auditory-eeg/1.0.0/'
 const dllink5='https://physionet.org/static/published-projects/auditory-eeg/auditory-evoked-potential-eeg-biometric-dataset-1.0.0.zip'
 
-//
-const dbKey1='adfecgdb'
-const dbKey2='aftdb'
-const dbKey3='aami-ec13'
-const dbKey4='apnea-ecg'
-const dbKey5='auditory-eeg'
+//keys to get the respective dataset with
+const dsKey1='adfecgdb'
+const dsKey2='aftdb'
+const dsKey3='aami-ec13'
+const dsKey4='apnea-ecg'
+const dsKey5='auditory-eeg'
 
 function DatasetsPage() {
-    function getRecordNum(inputCode){
-        fetch("/dataset/" + inputCode) // use "http://127.0.0.1:5000/dataset/" + inputCode for local'
-        .then(response => 
-          response.json()
-        )
-        .then(data => {
-          console.log(data.recordNum)
-          return data.recordNum
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }
-
-
     return (
         <Stack direction='column' alignItems="flex-start" justifyContent='flex-start' spacing={4}>
-            <DatasetItem dbname='ABDOMINAL AND DIRECT FETAL ECG' dblink={dblink1} dllink={dllink1} recordnum={getRecordNum(dbKey1)}/>
+            <DatasetItem dsname='ABDOMINAL AND DIRECT FETAL ECG' dslink={dslink1} dllink={dllink1} dskey={dsKey1}/>
 
-            <DatasetItem dbname='AF TERMINATION CHALLENGE' dblink={dblink2} dllink={dllink2} recordnum={getRecordNum(dbKey2)}/>
+            <DatasetItem dsname='AF TERMINATION CHALLENGE' dslink={dslink2} dllink={dllink2} dskey={dsKey2}/>
 
-            <DatasetItem dbname='ANSI/AAMI EC13 TEST WAVEFORMS' dblink={dblink3} dllink={dllink3} recordnum={getRecordNum(dbKey3)}/>
+            <DatasetItem dsname='ANSI/AAMI EC13 TEST WAVEFORMS' dslink={dslink3} dllink={dllink3} dskey={dsKey3}/>
 
-            <DatasetItem dbname='APNEA-ECG' dblink={dblink4} dllink={dllink4} recordnum={getRecordNum(dbKey4)}/>
+            <DatasetItem dsname='APNEA-ECG' dslink={dslink4} dllink={dllink4} dskey={dsKey4}/>
 
-            <DatasetItem dbname='AUDITORY EVOKED POTENTIAL EEG-BIOMETRIC' dblink={dblink5} dllink={dllink5} recordnum={getRecordNum(dbKey5)}/>
+            <DatasetItem dsname='AUDITORY EVOKED POTENTIAL EEG-BIOMETRIC' dslink={dslink5} dllink={dllink5} dskey={dsKey5}/>
         </Stack>
     );
 }
